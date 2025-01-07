447b727577773b39f6bd311c106f4cbb43bdf10e
Daniel Gak <dani_gak@hotmail.com>
2025-01-07 09:39:54 +0100

    feat: debug

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use ctrlc;
+use std::io::{Read, Write};
 use std::sync::mpsc::channel;
 
 fn main() {
@@ -31,5 +32,33 @@ fn run_steps_until_reaching_base(proven_base: u64) {
             }
         };
         pointer += 1;
+    }
+}
+
+// eg. echo "1200152" | cargo run
+
+fn debug_steps() {
+    let stdout = std::io::stdout();
+    let mut cout = std::io::BufWriter::new(stdout.lock());
+
+    let mut buf = Vec::new();
+    let stdin = std::io::stdin();
+    stdin.lock().read_to_end(&mut buf).unwrap();
+    let mut num = unsafe {
+        std::str::from_utf8_unchecked(&buf)
+            .trim()
+            .parse::<u64>()
+            .unwrap()
+    };
+
+    write!(cout, "{} ", num).unwrap();
+    while num > 1 {
+        if num & 1 != 0 {
+            num *= 3;
+            num += 1;
+        } else {
+            num /= 2;
+        }
+        write!(cout, "{} ", num).unwrap();
     }
 }
